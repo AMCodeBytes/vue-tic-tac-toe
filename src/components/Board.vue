43dae1d267b96b33fc02c136ef9786f6e9df2853
DatHighlander <57409463+DatHighlander@users.noreply.github.com>
2020-09-20 16:06:53 +0100

    Created the board view & select functionality

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,47 +1,70 @@
 <template>
-	<div class="container">
-		<h1>Board</h1>
-		<section>
-			<div class="row">
-				<div>0</div>
-				<div>1</div>
-				<div>2</div>
-			</div>
+	<v-container class="fill-height" fluid>
+		<v-row v-for="(row, i) in boardLayout" :key="i" class="text-center align-self-stretch" align="center" justify="center">
+			<v-col v-for="(column, j) in row" :key="j" @mouseover="setHover(true)" @mouseout="setHover(false)" @click="selectItem(i, j)" class="d-flex justify-center align-center elevation-1 align-self-stretch">
+				<template v-if="column">
+					{{ column }}
+				</template>
+				<template v-else>
+					<wbr>
+				</template>
+			</v-col>
+		</v-row>
 
-			<div class="row">
-				<div>3</div>
-				<div>4</div>
-				<div>5</div>
-			</div>
-			<div class="row">
-				<div>6</div>
-				<div>7</div>
-				<div>8</div>
-			</div>
-		</section>
-	</div>
+		<v-row justify="center">
+			<h1>Player: {{ player }}</h1>
+		</v-row>
+	</v-container>
 </template>
 
 <script>
+
 export default {
-	name: 'Board'
+	name: 'Board',
+	data: () => ({
+		boardLayout: [
+			[null, null, null],
+			[null, null, null],
+			[null, null, null]
+		],
+		player: 'X'
+	}),
+
+	computed: {
+		winCheck() {
+			// check if the current play has won
+		}
+	},
+
+	methods: {
+		selectItem(row, column) {
+			if (!this.boardLayout[row][column]) {
+				this.changeItem(row, column);
+				this.winCheck;
+				this.changePlayer();
+			}
+		},
+		setHover(bool) {
+			if (bool) {
+				event.target.classList.add('grey', 'lighten-3');
+			} else {
+				event.target.classList.remove('grey', 'lighten-3');
+			}
+		},
+		changeItem(row, column) {
+			this.boardLayout[row][column] = this.player;
+		},
+		changePlayer() {
+			this.player = this.player === 'X' ? 'O' : 'X';
+		}
+	}
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-	margin: 40px 0 0;
-}
-ul {
-	list-style-type: none;
-	padding: 0;
-}
-li {
-	display: inline-block;
-	margin: 0 10px;
-}
-a {
-	color: #42b983;
+.col {
+	cursor: pointer;
+	font-size: 32px;
 }
 </style>

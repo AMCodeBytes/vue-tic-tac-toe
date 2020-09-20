2ae7e314c750b2478a75cb0dd0f5c26f639cb55c
DatHighlander <57409463+DatHighlander@users.noreply.github.com>
2020-09-20 11:49:36 +0100

    Just commiting some extra boilerplate setup code.

---
+++ src/components/Board.vue
@@ -0,0 +1,47 @@
+<template>
+	<div class="container">
+		<h1>Board</h1>
+		<section>
+			<div class="row">
+				<div>0</div>
+				<div>1</div>
+				<div>2</div>
+			</div>
+
+			<div class="row">
+				<div>3</div>
+				<div>4</div>
+				<div>5</div>
+			</div>
+			<div class="row">
+				<div>6</div>
+				<div>7</div>
+				<div>8</div>
+			</div>
+		</section>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Board'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+	margin: 40px 0 0;
+}
+ul {
+	list-style-type: none;
+	padding: 0;
+}
+li {
+	display: inline-block;
+	margin: 0 10px;
+}
+a {
+	color: #42b983;
+}
+</style>
